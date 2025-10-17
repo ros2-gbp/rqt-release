5642fdfcda82664d316611550d6b60a06933c2b7
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-17 12:16:32 +0200

    Rebase from 'upstream'

--- rqt/setup.py
+++ rqt/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt'
 setup(
     name=package_name,
-    version='1.10.1',
+    version='1.10.2',
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -17,7 +17,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
