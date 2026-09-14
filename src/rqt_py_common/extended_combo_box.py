c9487d43ccddf9410f96813f4b94da122b11e21e
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-09-14 19:34:57 +0200

    Rebase from 'debian/rolling/rqt_py_common'

--- src/rqt_py_common/extended_combo_box.py
+++ src/rqt_py_common/extended_combo_box.py
@@ -30,10 +30,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-try:
-    from python_qt_binding.QtCore import QSortFilterProxyModel  # Qt 5
-except ImportError:
-    from python_qt_binding.QtGui import QSortFilterProxyModel  # Qt 4
+from python_qt_binding.QtCore import QSortFilterProxyModel
 from python_qt_binding.QtCore import Qt, Signal, Slot
 from python_qt_binding.QtWidgets import QComboBox, QCompleter
 
